5187cc44ea639d56b6e117669e4d410ae491f00b
Kristopher Prince <kristopher.prince@gmail.com>
2020-05-19 11:26:43 -0500

    Updates

---
+++ precache-manifest.52d8fe89cee139ec1fb1759a2e66a40c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d97425472827d3965b34d7f26e309be",
+    "url": "/cy-app/index.html"
+  },
+  {
+    "revision": "312e2d9216c5d3292883",
+    "url": "/cy-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02ac56277cc13e3f1cb5",
+    "url": "/cy-app/static/js/2.b6021e7d.chunk.js"
+  },
+  {
+    "revision": "0b82a8467a350fa3648925119144698a",
+    "url": "/cy-app/static/js/2.b6021e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312e2d9216c5d3292883",
+    "url": "/cy-app/static/js/main.7248ea53.chunk.js"
+  },
+  {
+    "revision": "6d7635220c213a5fd2a9",
+    "url": "/cy-app/static/js/runtime-main.95ad4c85.js"
+  }
+]);
